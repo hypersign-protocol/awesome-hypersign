545c0ce19b3f24716def6f23cd6c3c881629c8c8
Raj6939 <raj.v.patil108@gmail.com>
2023-06-08 16:30:59 +0530

    added route changes

--- src/components/shared/FooterCopyright.vue
+++ src/components/shared/FooterCopyright.vue
@@ -25,7 +25,7 @@ export default {
 			</a>
 			. Developed by
 			<a
-				href="https://hypersing.id"
+				href="https://hypersign.id"
 				target="__blank"
 				class="font-general-medium text-secondary-dark dark:text-secondary-light uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500"
 				>{{ author }}</a
